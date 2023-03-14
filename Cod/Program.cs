124a4a892f7b437f7949cdc1d357fce74fbc813b
Alex Ru <Alex.ru.tver@gmail.com>
2023-03-15 00:42:44 +0300

    Создаём массив из массива из элементов нужного размера

--- Cod/Program.cs
+++ Cod/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿//Напишите программу которая из имеющегося массива строк создаёт
+//  массив в котором все строки меньше иле ровны
+// трём символам. 
+
+Console.Clear();
+
+string[] originalArray = { "hello", "123", "string", "hed", "5", "!.&", "homework" };
+
+
+
+string[] GetNewArray(string[] array)
+{
+    int size = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if(array[j].Length <= 3)
+        size++;
+    }
+
+    string[] newArray = new string[size];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+    return newArray;
+}
+
+void PrintArray(string[] inarray) 
+{
+    for (int i = 0; i  <inarray.Length; i++)
+    {
+        Console.Write($"{inarray[i]} ");
+    }
+}
+
+string[] result = GetNewArray(originalArray);
+System.Console.Write("[ ");
+PrintArray(result);
+System.Console.WriteLine("]");
+
+
